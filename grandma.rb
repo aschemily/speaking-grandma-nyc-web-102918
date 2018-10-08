ec0829012ac0418e931ccff6d4264225f92fb7d3
Emily Asch <e.asch@me.com>
2018-10-08 22:40:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,9 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-  return "NO; NOT SINCE 1938!"
-  
+  return "NO, NOT SINCE 1938!"
+else
+  return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
